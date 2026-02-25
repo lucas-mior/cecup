77ea27a95d8815ce526066af8084bb14b5d1638c
lucas-mior <lucas.mior@tutamail.com>
2026-02-24 22:18:42 -0300

    wip

--- config.c
+++ config.c
@@ -1,3 +1,6 @@
+#if !defined(CONFIG_C)
+#define CONFIG_C
+
 #include "cecup.h"
 
 static void
@@ -63,3 +66,52 @@ read_config(void) {
     }
     g_key_file_free(key);
 }
+
+static void
+save_config(void) {
+    GKeyFile *key;
+    char *out;
+    gsize len;
+
+    key = g_key_file_new();
+    g_key_file_set_string(key, "Paths", "src",
+                          gtk_entry_get_text(GTK_ENTRY(cecup_state.src_entry)));
+    g_key_file_set_string(key, "Paths", "dst",
+                          gtk_entry_get_text(GTK_ENTRY(cecup_state.dst_entry)));
+    g_key_file_set_string(
+        key, "Tools", "diff",
+        gtk_entry_get_text(GTK_ENTRY(cecup_state.diff_entry)));
+    g_key_file_set_string(
+        key, "Tools", "term",
+        gtk_entry_get_text(GTK_ENTRY(cecup_state.term_entry)));
+    g_key_file_set_boolean(key, "Filters", "new",
+                           gtk_toggle_button_get_active(
+                               GTK_TOGGLE_BUTTON(cecup_state.filter_new)));
+    g_key_file_set_boolean(key, "Filters", "hard",
+                           gtk_toggle_button_get_active(
+                               GTK_TOGGLE_BUTTON(cecup_state.filter_hard)));
+    g_key_file_set_boolean(key, "Filters", "update",
+                           gtk_toggle_button_get_active(
+                               GTK_TOGGLE_BUTTON(cecup_state.filter_update)));
+    g_key_file_set_boolean(key, "Filters", "equal",
+                           gtk_toggle_button_get_active(
+                               GTK_TOGGLE_BUTTON(cecup_state.filter_equal)));
+    g_key_file_set_boolean(key, "Filters", "delete",
+                           gtk_toggle_button_get_active(
+                               GTK_TOGGLE_BUTTON(cecup_state.filter_delete)));
+    g_key_file_set_boolean(key, "Filters", "ignore",
+                           gtk_toggle_button_get_active(
+                               GTK_TOGGLE_BUTTON(cecup_state.filter_ignore)));
+    g_key_file_set_boolean(key, "Options", "check_fs",
+                           gtk_toggle_button_get_active(
+                               GTK_TOGGLE_BUTTON(cecup_state.check_fs_toggle)));
+
+    out = g_key_file_to_data(key, &len, NULL);
+    g_file_set_contents(cecup_state.config_path, out, (gssize)len, NULL);
+
+    g_free(out);
+    g_key_file_free(key);
+    return;
+}
+
+#endif /* CONFIG_C */
--- hooks.c
+++ hooks.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "cecup.h"
 #include "rsync.c"
+#include "config.c"
 
 static void
 free_cecup_row(gpointer data) {
@@ -416,53 +417,6 @@ on_menu_exclude_dir(GtkWidget *m, gpointer data) {
 
     g_free(ud->filepath);
     g_free(ud);
-    return;
-}
-
-static void
-save_config(void) {
-    GKeyFile *key;
-    char *out;
-    gsize len;
-
-    key = g_key_file_new();
-    g_key_file_set_string(key, "Paths", "src",
-                          gtk_entry_get_text(GTK_ENTRY(cecup_state.src_entry)));
-    g_key_file_set_string(key, "Paths", "dst",
-                          gtk_entry_get_text(GTK_ENTRY(cecup_state.dst_entry)));
-    g_key_file_set_string(
-        key, "Tools", "diff",
-        gtk_entry_get_text(GTK_ENTRY(cecup_state.diff_entry)));
-    g_key_file_set_string(
-        key, "Tools", "term",
-        gtk_entry_get_text(GTK_ENTRY(cecup_state.term_entry)));
-    g_key_file_set_boolean(key, "Filters", "new",
-                           gtk_toggle_button_get_active(
-                               GTK_TOGGLE_BUTTON(cecup_state.filter_new)));
-    g_key_file_set_boolean(key, "Filters", "hard",
-                           gtk_toggle_button_get_active(
-                               GTK_TOGGLE_BUTTON(cecup_state.filter_hard)));
-    g_key_file_set_boolean(key, "Filters", "update",
-                           gtk_toggle_button_get_active(
-                               GTK_TOGGLE_BUTTON(cecup_state.filter_update)));
-    g_key_file_set_boolean(key, "Filters", "equal",
-                           gtk_toggle_button_get_active(
-                               GTK_TOGGLE_BUTTON(cecup_state.filter_equal)));
-    g_key_file_set_boolean(key, "Filters", "delete",
-                           gtk_toggle_button_get_active(
-                               GTK_TOGGLE_BUTTON(cecup_state.filter_delete)));
-    g_key_file_set_boolean(key, "Filters", "ignore",
-                           gtk_toggle_button_get_active(
-                               GTK_TOGGLE_BUTTON(cecup_state.filter_ignore)));
-    g_key_file_set_boolean(key, "Options", "check_fs",
-                           gtk_toggle_button_get_active(
-                               GTK_TOGGLE_BUTTON(cecup_state.check_fs_toggle)));
-
-    out = g_key_file_to_data(key, &len, NULL);
-    g_file_set_contents(cecup_state.config_path, out, (gssize)len, NULL);
-
-    g_free(out);
-    g_key_file_free(key);
     return;
 }
 
