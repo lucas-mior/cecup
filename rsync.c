0a892965b4ce946e3d6b8ba7b9f759f810b18c0f
lucas-mior <lucas.mior@tutamail.com>
2026-03-02 00:25:17 -0300

    cleanup

--- rsync.c
+++ rsync.c
@@ -117,12 +117,11 @@ dispatch_log(char *format, ...) {
 
 static gboolean
 log_error_handler(void *user_data) {
-    UIUpdateData *data;
+    UIUpdateData *data = user_data;
     GtkTextIter end;
     GtkTextTagTable *table;
     GtkTextTag *tag;
 
-    data = user_data;
     table = gtk_text_buffer_get_tag_table(cecup.log_buffer);
     if ((tag = gtk_text_tag_table_lookup(table, "err_red")) == NULL) {
         gtk_text_buffer_create_tag(cecup.log_buffer, "err_red", "foreground",
