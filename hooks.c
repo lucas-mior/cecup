ff0fc168cceeccd832239ef2b16967d5027cd603
lucas-mior <lucas.mior@tutamail.com>
2026-03-02 12:25:09 -0300

    style

--- hooks.c
+++ hooks.c
@@ -1523,9 +1523,8 @@ update_ui_handler(void *user_data) {
                  && strcmp(row->src_path, data->filepath) == 0)
                 || (row->dst_path_len == data->filepath_length && row->dst_path
                     && strcmp(row->dst_path, data->filepath) == 0)) {
-                for (int32 j_idx = i; j_idx < cecup.rows_count - 1;
-                     j_idx += 1) {
-                    cecup.rows[j_idx] = cecup.rows[j_idx + 1];
+                for (int32 j = i; j < cecup.rows_count - 1; j += 1) {
+                    cecup.rows[j] = cecup.rows[j + 1];
                 }
                 cecup.rows_count -= 1;
                 break;
