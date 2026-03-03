e2659d1be39ef880c384aed3b2d84ac707adbe3e
lucas-mior <lucas.mior@tutamail.com>
2026-03-03 19:53:43 -0300

    refactor dispatch log and error

--- rsync.c
+++ rsync.c
@@ -78,19 +78,40 @@ enum RsyncCharAttribute {
 #define BATCH_SIZE 256
 
 static void
-dispatch_log(char *format, ...) {
+dispatch_log_internal(enum DataType type, char *format, va_list va_args) {
     UIUpdateData *data;
-    va_list va_args;
     char buffer[8192];
     int64 n;
+    int64 last_whitespace_index;
+    int64 current_column;
 
-    va_start(va_args, format);
     n = vsnprintf(buffer, SIZEOF(buffer), format, va_args);
-    va_end(va_args);
 
     if ((n <= 0) || (n >= SIZEOF(buffer))) {
         error("Error in vsnprintf(%s) (n = %lld)\n", format, (llong)n);
         exit(EXIT_FAILURE);
+    }
+
+    last_whitespace_index = -1;
+    current_column = 0;
+    for (int64 i = 0; i < n; i += 1) {
+        if ((buffer[i] == ' ') || (buffer[i] == '\t') || (buffer[i] == '\n')) {
+            last_whitespace_index = i;
+        }
+
+        if (buffer[i] == '\n') {
+            current_column = 0;
+        } else {
+            current_column += 1;
+        }
+
+        if (current_column > 120) {
+            if (last_whitespace_index != -1) {
+                buffer[last_whitespace_index] = '\n';
+                current_column = i - last_whitespace_index;
+                last_whitespace_index = -1;
+            }
+        }
     }
 
     g_mutex_lock(&cecup.ui_arena_mutex);
@@ -102,38 +123,28 @@ dispatch_log(char *format, ...) {
     memcpy64(data->message, buffer, n + 1);
     g_mutex_unlock(&cecup.ui_arena_mutex);
 
-    data->type = DATA_TYPE_LOG;
+    data->type = type;
     g_idle_add(update_ui_handler, data);
     return;
 }
 
 static void
-dispatch_log_error(char *format, ...) {
-    UIUpdateData *data;
+dispatch_log(char *format, ...) {
     va_list va_args;
-    char buffer[8192];
-    int64 n;
 
     va_start(va_args, format);
-    n = vsnprintf(buffer, SIZEOF(buffer), format, va_args);
+    dispatch_log_internal(DATA_TYPE_LOG, format, va_args);
     va_end(va_args);
+    return;
+}
 
-    if ((n <= 0) || (n >= SIZEOF(buffer))) {
-        error("Error in vsnprintf(%s) (n = %lld)\n", format, (llong)n);
-        exit(EXIT_FAILURE);
-    }
+static void
+dispatch_log_error(char *format, ...) {
+    va_list va_args;
 
-    g_mutex_lock(&cecup.ui_arena_mutex);
-    data = xarena_push(cecup.ui_arena, ALIGN16(SIZEOF(UIUpdateData)));
-    memset64(data, 0, SIZEOF(UIUpdateData));
-
-    data->message_len = n;
-    data->message = xarena_push(cecup.ui_arena, ALIGN16(n + 1));
-    memcpy64(data->message, buffer, n + 1);
-    g_mutex_unlock(&cecup.ui_arena_mutex);
-
-    data->type = DATA_TYPE_LOG_ERROR;
-    g_idle_add(update_ui_handler, data);
+    va_start(va_args, format);
+    dispatch_log_internal(DATA_TYPE_LOG_ERROR, format, va_args);
+    va_end(va_args);
     return;
 }
 
