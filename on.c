875d9c2f1e628dafde021c9dc22fc80aceb5de80
lucas-mior <lucas.mior@tutamail.com>
2026-03-12 03:03:29 -0300

    style

--- on.c
+++ on.c
@@ -39,15 +39,15 @@ typedef struct {
 // Note: NEVER delete lines with // clang-format
 // clang-format off
 static CecupMenuItem context_menu_items[] = {
-{N_("📄 Open File"),          0,          0,                                  on_menu_open_item, "file"},
-{N_("📂 Open Folder"),        0,          0,                                  on_menu_open_item, "folder"},
-{N_("📋 Copy Relative Path"), GDK_KEY_c,  GDK_CONTROL_MASK,                   on_menu_copy_path, "relative"},
+{N_("📄 Open File"),          0,          0,                                 on_menu_open_item, "file"},
+{N_("📂 Open Folder"),        0,          0,                                 on_menu_open_item, "folder"},
+{N_("📋 Copy Relative Path"), GDK_KEY_c,  GDK_CONTROL_MASK,                  on_menu_copy_path, "relative"},
 {N_("📍 Copy Full Path"),     GDK_KEY_c,  GDK_CONTROL_MASK | GDK_SHIFT_MASK, on_menu_copy_path, "absolute"},
-{N_("⏯️ Apply"),              0,          0,                                  on_menu_apply,      NULL},
-{N_("🔍 Diff"),                0,          0,                                  on_menu_diff,       NULL},
-{N_("✏️ Rename"),              GDK_KEY_F2, 0,                                  on_menu_rename,     NULL},
-{N_("🗑️ Delete"),              0,          0,                                  on_menu_delete,     NULL},
-{N_("💤 Ignore..."),          0,          0,                                  NULL,                NULL},
+{N_("⏯️ Apply"),              0,          0,                                 on_menu_apply,     NULL},
+{N_("🔍 Diff"),               0,          0,                                 on_menu_diff,      NULL},
+{N_("✏️ Rename"),              GDK_KEY_F2, 0,                                 on_menu_rename,    NULL},
+{N_("🗑️ Delete"),             0,          0,                                 on_menu_delete,    NULL},
+{N_("💤 Ignore..."),          0,          0,                                 NULL,              NULL},
 };
 // clang-format on
 
