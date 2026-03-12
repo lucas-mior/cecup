302d441d25a0856a8b3fd9b20c32d078c6ad8c59
lucas-mior <lucas.mior@tutamail.com>
2026-03-12 20:54:25 -0300

    style

--- on.c
+++ on.c
@@ -1306,14 +1306,14 @@ on_path_edited(GtkCellRendererText *renderer, char *path_str, char *new_text,
     if (side == SIDE_LEFT) {
         base_path = cecup.src_base;
         base_path_len = cecup.src_base_len;
-        if (!row->src_path) {
+        if (row->src_path == NULL) {
             goto out;
         }
         SNPRINTF(relative_old, "/%s", row->src_path);
     } else {
         base_path = cecup.dst_base;
         base_path_len = cecup.dst_base_len;
-        if (!row->dst_path) {
+        if (row->dst_path == NULL) {
             goto out;
         }
         SNPRINTF(relative_old, "/%s", row->dst_path);
