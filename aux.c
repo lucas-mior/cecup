c18ebada20c3e5388a353f49331f3fd23e2b4fae
lucas-mior <lucas.mior@tutamail.com>
2026-03-18 14:38:13 -0300

    use memcmp

--- aux.c
+++ aux.c
@@ -526,7 +526,7 @@ update_ui_handler(void *data) {
                 }
             } else {
                 if (row->path_len == pattern_len) {
-                    if (!strcmp(path_test, pattern)) {
+                    if (memcmp64(path_test, pattern, pattern_len)) {
                         match = true;
                     }
                 }
