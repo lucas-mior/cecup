45bda87e42a0a9a2b7805ebe42488f15ae358f37
lucas-mior <lucas.mior@tutamail.com>
2026-03-18 16:44:54 -0300

    copy absolute path by default

--- on.c
+++ on.c
@@ -46,8 +46,8 @@ typedef struct {
 static CecupMenuItem context_menu_items[] = {
 {N_("📄 Open File"),          0,          0,                                 on_menu_open_item, "file"},
 {N_("📂 Open Folder"),        0,          0,                                 on_menu_open_item, "folder"},
-{N_("📋 Copy Relative Path"), GDK_KEY_c,  GDK_CONTROL_MASK,                  on_menu_copy_path, "relative"},
-{N_("📍 Copy Full Path"),     GDK_KEY_c,  GDK_CONTROL_MASK | GDK_SHIFT_MASK, on_menu_copy_path, "absolute"},
+{N_("📍 Copy Full Path"),     GDK_KEY_c,  GDK_CONTROL_MASK,                  on_menu_copy_path, "absolute"},
+{N_("📋 Copy Relative Path"), GDK_KEY_c,  GDK_CONTROL_MASK | GDK_SHIFT_MASK, on_menu_copy_path, "relative"},
 {N_("⏯️ Apply"),              0,          0,                                 on_menu_apply,     NULL},
 {N_("🔍 Diff"),               0,          0,                                 on_menu_diff,      NULL},
 {N_("✏️ Rename"),              GDK_KEY_F2, 0,                                 on_menu_rename,    NULL},
