6b245e7adc1ea36e53c51bb63f5fc21146668fd5
lucas-mior <lucas.mior@tutamail.com>
2026-03-19 20:31:05 -0300

    WIP

--- enums.h
+++ enums.h
@@ -23,31 +23,28 @@
 #define ENUM_ELEMENT(...) SELECT_ON_NUM_ARGS(ENUM_ELEMENT_HANDLER_, __VA_ARGS__)
 
 #if !defined(GENERATE_ENUM_STRINGS)
-  #define BEGIN_ENUM(ENUM_NAME) enum ENUM_NAME {
+  #define BEGIN_ENUM(ENUM_NAME)           enum ENUM_NAME {
 
   #define ENUM_ELEMENT_HANDLER_1(e)       CAT(CAT(ENUM_NAME_LOCAL, _), e),
   #define ENUM_ELEMENT_HANDLER_2(e, v)    CAT(CAT(ENUM_NAME_LOCAL, _), e), 
   #define ENUM_ELEMENT_HANDLER_3(e, v, s) CAT(CAT(ENUM_NAME_LOCAL, _), e) = v,
 
   #define END_ENUM(ENUM_NAME)             CAT(CAT(ENUM_NAME_LOCAL, _), LAST) \
-      }; \
-      char *enum_string_##ENUM_NAME(int32 index);
+                                          }; \
+                                          char *enum_string_##ENUM_NAME(int index);
 #else
-  #define BEGIN_ENUM(ENUM_NAME) char *enum_string_##ENUM_NAME(int32 index) {\
-      switch (index) {
+  #define BEGIN_ENUM(ENUM_NAME)           char *enum_string_##ENUM_NAME(int index) { \
+                                          switch (index) {
 
-  #define ENUM_ELEMENT_HANDLER_1(e) \
-      case CAT(CAT(ENUM_NAME_LOCAL, _), e): return QUOTE(ENUM_NAME_LOCAL) "_" #e;
-
-  #define ENUM_ELEMENT_HANDLER_2(e, v) \
-      case CAT(CAT(ENUM_NAME_LOCAL, _), e): \
-          return _Generic((v), \
-              char*: (char*)(v), \
-              default: QUOTE(ENUM_NAME_LOCAL) "_" #e \
-          );
-
-  #define ENUM_ELEMENT_HANDLER_3(e, v, s) \
-      case CAT(CAT(ENUM_NAME_LOCAL, _), e): return s;
+  #define ENUM_ELEMENT_HANDLER_1(e)       case CAT(CAT(ENUM_NAME_LOCAL, _), e): \
+                                              return QUOTE(ENUM_NAME_LOCAL) "_" #e;
+  #define ENUM_ELEMENT_HANDLER_2(e, v)    case CAT(CAT(ENUM_NAME_LOCAL, _), e): \
+                                              return _Generic((v), \
+                                                  char*: (char*)(v), \
+                                                  default: QUOTE(ENUM_NAME_LOCAL) "_" #e \
+                                              );
+  #define ENUM_ELEMENT_HANDLER_3(e, v, s) case CAT(CAT(ENUM_NAME_LOCAL, _), e): \
+                                              return s;
 
   #define END_ENUM(ENUM_NAME) default: return "Unknown value"; \
       } \
