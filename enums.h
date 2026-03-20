94c334f6439f23df0ca3d6784533035e9faf5c36
lucas-mior <lucas.mior@tutamail.com>
2026-03-19 22:01:52 -0300

    style

--- enums.h
+++ enums.h
@@ -30,10 +30,10 @@ _Static_assert((ENUM_GENERATE_STRINGS == 0) || (ENUM_GENERATE_STRINGS == 1),
 #pragma clang diagnostic pop
 
 #if !defined(CAT) || !defined(CAT3)
-  #define CAT_(a, b) a##b
-  #define CAT(a, b) CAT_(a, b)
+  #define CAT_(a, b)     a##b
   #define CAT3_(a, b, c) a##b##c
-  #define CAT3(a, b, c) CAT3_(a, b, c)
+  #define CAT(a, b)      CAT_(a, b)
+  #define CAT3(a, b, c)  CAT3_(a, b, c)
 #endif
 
 #define NUM_ARGS_(_1, _2, _3, _4, _5, _6, _7, _8, n, ...) n
