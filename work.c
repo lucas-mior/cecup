7429010e641ffec98c6a46560e2a4ae206d44529
lucas-mior <lucas.mior@tutamail.com>
2026-03-25 15:53:21 -0300

    CTIME only matters for dirs

--- work.c
+++ work.c
@@ -728,7 +728,7 @@ work_preview(void *user_data) {
                             reason |= REASON_MTIME;
                             attributes_differ = true;
                         }
-                        if (stat_src->st_ctime != stat_dst->st_ctime) {
+                        if (is_dir && (stat_src->st_ctime != stat_dst->st_ctime)) {
                             reason |= REASON_CTIME;
                             attributes_differ = true;
                         }
