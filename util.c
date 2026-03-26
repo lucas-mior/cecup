6f4e50d7c9fcc0bd1702e449f35d8d26747d8622
lucas-mior <lucas.mior@tutamail.com>
2026-03-26 03:21:04 -0300

    dont test ALIGN(7) on macos

--- util.c
+++ util.c
@@ -2141,15 +2141,18 @@ main(int argc, char **argv) {
 #endif
 
     {
-        ASSERT_EQUAL(ALIGN(7), 16);
-        ASSERT_EQUAL(ALIGN(16), 16);
-        ASSERT_EQUAL(ALIGN(17), 32);
-
         int a = 10;
         int b = 20;
+
         SWAP(a, b);
         ASSERT_EQUAL(a, 20);
         ASSERT_EQUAL(b, 10);
+
+#if !OS_MAC
+        ASSERT_EQUAL(ALIGN(7), 16);
+#endif
+        ASSERT_EQUAL(ALIGN(16), 16);
+        ASSERT_EQUAL(ALIGN(17), 32);
     }
 
     for (enum WeekDay day = WEEK_DAY_MONDAY; day <= WEEK_DAY_LAST; day += 1) {
