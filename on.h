90358585d8e6af92b0fb23b0067563c0b7f52183
lucas-mior <lucas.mior@tutamail.com>
2026-03-27 23:24:53 -0300

    on_tree.c split

--- on.h
+++ on.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025 Mior, Lucas;
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the*License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #if !defined(ON_H)
 #define ON_H
 
@@ -14,6 +31,7 @@ static void on_menu_rename(GtkWidget *m, void *data);
 static void on_menu_delete(GtkWidget *m, void *data);
 
 static void on_popover_closed(GtkWidget *popover, void *data);
+static void execute_menu_item(GtkWidget *tree, CecupMenuItem *menu_item);
 
 static CecupMenuItem tree_menu_items[] = {
 {N_("📄 Open File"),          0,          0,                                 on_menu_open_item, "file"},
