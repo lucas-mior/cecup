6e2e28489c466ab5f8ecde0c58d613b48fe52daa
lucas-mior <lucas.mior@tutamail.com>
2026-03-27 23:20:51 -0300

    cecuprowcompare

--- update.c
+++ update.c
@@ -30,6 +30,7 @@
 #include "list_model.c"
 #include "ignore_patterns.c"
 #include "on_path.c"
+#include "item.c"
 
 #if defined(__INCLUDE_LEVEL__) && (__INCLUDE_LEVEL__ == 0)
 #define TESTING_update 1
