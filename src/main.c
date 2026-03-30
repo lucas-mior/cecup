de239dcb1698223d6046bd423e4fac5a9ee218cc
lucas-mior <lucas.mior@tutamail.com>
2026-03-30 18:24:08 -0300

    why though

--- src/main.c
+++ src/main.c
@@ -540,6 +540,11 @@ main_application_run(GtkApplication *application, gpointer user_data) {
     gtk_paned_set_resize_start_child(GTK_PANED(v_paned_inner), TRUE);
     gtk_paned_set_shrink_start_child(GTK_PANED(v_paned_inner), TRUE);
 
+    gtk_paned_set_resize_end_child(GTK_PANED(v_paned_outer), TRUE);
+    gtk_paned_set_shrink_end_child(GTK_PANED(v_paned_outer), TRUE);
+    gtk_paned_set_resize_start_child(GTK_PANED(v_paned_outer), TRUE);
+    gtk_paned_set_shrink_start_child(GTK_PANED(v_paned_outer), TRUE);
+
     gtk_widget_set_name(v_paned_inner, "v_paned_inner");
     gtk_widget_set_name(v_paned_outer, "v_paned_outer");
 
