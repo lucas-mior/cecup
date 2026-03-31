6a6d00c70065fc5b25287e8b4e8e1d0f2a2149e9
lucas-mior <lucas.mior@tutamail.com>
2026-03-31 20:20:26 -0300

    style

--- src/update.c
+++ src/update.c
@@ -377,7 +377,8 @@ update_row_rename(Message *message) {
             n_idx = traversal_push(traversal, &traversal->stats[idx],
                                    path_new, new_path_len + suffix_len,
                                    traversal->link_targets[idx],
-                                   traversal->link_targets_lens[idx], p_match_str, p_match_len);
+                                   traversal->link_targets_lens[idx],
+                                   p_match_str, p_match_len);
         }
 
         merge_row_id = -1;
