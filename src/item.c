2d7acb7b12dffb80a0547880bdb13c58409dd707
lucas-mior <lucas.mior@tutamail.com>
2026-04-01 04:21:14 -0300

    names

--- src/item.c
+++ src/item.c
@@ -405,26 +405,26 @@ typedef void(*SortFunction)(SortEntry *a, int64);
 
 #define i_key SortEntry
 #define i_cmp(a,b) cecup_item_compare_string_key(a,b)
-#define T compare_string_sort
+#define T compare_string
 #include "stc/sort.h"
 
 #define i_key SortEntry
 #define i_cmp(a,b) cecup_item_compare_int_key(a,b)
-#define T compare_int_sort
+#define T compare_int
 #include "stc/sort.h"
 
 static SortFunction compare_item_functions[] = {
-    [COL_SELECTED]   = compare_int_sort_sort,
-    [COL_SRC_ACTION] = compare_int_sort_sort,
-    [COL_DST_ACTION] = compare_int_sort_sort,
-    [COL_SRC_PATH]   = compare_string_sort_sort,
-    [COL_DST_PATH]   = compare_string_sort_sort,
-    [COL_SIZE_TEXT]  = compare_int_sort_sort,
-    [COL_SIZE_RAW]   = compare_int_sort_sort,
-    [COL_MTIME_TEXT] = compare_int_sort_sort,
-    [COL_MTIME_RAW]  = compare_int_sort_sort,
-    [COL_ROW_ID]     = compare_int_sort_sort,
-    [NUM_COLS]       = compare_int_sort_sort,
+    [COL_SELECTED]   = compare_int_sort,
+    [COL_SRC_ACTION] = compare_int_sort,
+    [COL_DST_ACTION] = compare_int_sort,
+    [COL_SRC_PATH]   = compare_string_sort,
+    [COL_DST_PATH]   = compare_string_sort,
+    [COL_SIZE_TEXT]  = compare_int_sort,
+    [COL_SIZE_RAW]   = compare_int_sort,
+    [COL_MTIME_TEXT] = compare_int_sort,
+    [COL_MTIME_RAW]  = compare_int_sort,
+    [COL_ROW_ID]     = compare_int_sort,
+    [NUM_COLS]       = compare_int_sort,
 };
 
 #if (0 == TESTING_item) && TESTING
