a2783648bc2b6d28c9b673a5f04e6935b1a807ff
lucas-mior <lucas.mior@tutamail.com>
2026-04-02 17:10:39 -0300

    use traversal_unlink in both cases of update_row_remove

--- src/update.c
+++ src/update.c
@@ -75,20 +75,7 @@ update_row_remove(Message *message) {
 
         changed = true;
 
-        if (S_ISREG(traversal->stats[idx].st_mode)
-                && (traversal->stats[idx].st_nlink > 1)) {
-            char inode[32];
-            int32 inode_len;
-            int32 *first_idx_ptr;
-
-            inode_len = ITOA(inode, (long)traversal->stats[idx].st_ino);
-            if ((first_idx_ptr = hash_lookup_inode_map(traversal->inode_map, inode, inode_len))) {
-                traversal->nlinks[*first_idx_ptr] -= 1;
-                if (traversal->nlinks[*first_idx_ptr] <= 0) {
-                    hash_remove_inode_map(traversal->inode_map, inode, inode_len);
-                }
-            }
-        }
+        traversal_unlink(traversal, idx);
 
         hash_remove_fs_map(traversal->map,
                            traversal->paths[idx], traversal->paths_lens[idx]);
