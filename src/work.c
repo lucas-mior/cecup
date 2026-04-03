ed4db2a775d012c0be9a193bb8d693085c7c2958
lucas-mior <lucas.mior@tutamail.com>
2026-04-02 23:17:14 -0300

    use traversal_add_link from work.c creation

--- src/work.c
+++ src/work.c
@@ -256,30 +256,7 @@ work_traverse_fs(Traversal *traversal) {
         if ((ent->fts_info == FTS_F)
              && (ent->fts_statp->st_nlink > 1)
              && !matched_pattern) {
-            char inode[32];
-            int32 inode_len;
-            HardLinkList **first_link_ptr;
-            HardLinkList *new_link;
-
-            inode_len = ITOA(inode, (long)ent->fts_statp->st_ino);
-            if ((first_link_ptr = hash_lookup_inode_map(traversal->inode_map, inode, inode_len))) {
-                first_link = *first_link_ptr;
-
-                new_link = xarena_push(traversal->arena, SIZEOF(*new_link));
-                new_link->name = path;
-                new_link->name_len = path_len;
-                new_link->idx = traversal->nfiles;
-                new_link->next = NULL;
-
-                hard_link_append(first_link, new_link);
-            } else {
-                first_link = xarena_push(traversal->arena, SIZEOF(*first_link));
-                first_link->name = path;
-                first_link->name_len = path_len;
-                first_link->idx = traversal->nfiles;
-                first_link->next = NULL;
-                hash_insert_inode_map(traversal->inode_map, inode, inode_len, first_link);
-            }
+            first_link = traversal_add_link(traversal, *(ent->fts_statp), path, path_len);
         }
 
         traversal_push(traversal, ent->fts_statp,
