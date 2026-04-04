a6db19bba26677573bc7e9c512558b364123cefd
lucas-mior <lucas.mior@tutamail.com>
2026-04-04 16:59:12 -0300

    wip

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 if [[ "${1:-}" != "--wrapped" ]]; then
     # Filter out lines starting with '+' followed by '[' or '[['
-    pattern="^ +'\['.+'\]'"
+    pattern="^\+ '\['.+'\]'"
     "$0" --wrapped "$@" 2> >(grep -Ev "$pattern" >&2)
     exit $?
 fi
