cb445e1c52cd9ffc9e0d4ad5d5fcc2dc733d4cc6
lucas-mior <lucas.mior@tutamail.com>
2026-04-04 22:29:43 -0300

    WIP

--- src/update.c
+++ src/update.c
@@ -378,8 +378,6 @@ update_row_transfer(char *path_transfered, int32 path_transfered_len) {
     if (cecup.rows[R][row_id] < 0) {
         int32 idx;
 
-        error("Case %s idx < 0 (new file)\n", path_transfered);
-
         if ((hash_lookup_fs_map(traversal_dst->map, path_transfered, path_transfered_len, &idx))) {
             cecup.rows[R][row_id] = idx;
         } else {
@@ -405,8 +403,6 @@ update_row_transfer(char *path_transfered, int32 path_transfered_len) {
         traversal_dst->row_ids[cecup.rows[R][row_id]] = row_id;
     } else {
         int32 idx = cecup.rows[R][row_id];
-
-        error("Case %s idx >= 0 (just update)\n", path_transfered);
 
         if (S_ISLNK(stat.st_mode)) {
             symlink_target_len = traversal_symlink_get(traversal_dst, full_path, &symlink_target);
