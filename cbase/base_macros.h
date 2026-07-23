02a9745c390b5a58756d2657a37d2abf844a8995
lucas-mior <lucas.mior@tutamail.com>
2026-07-23 16:39:24 -0300

    cleanup

--- cbase/base_macros.h
+++ cbase/base_macros.h
@@ -156,7 +156,7 @@ _Generic((SIZE), \
 #define π 3.14159265358979323846264338327950288
 
 #if !defined(__has_builtin)
-  #define __has_builtin(x) 0  // Compatibility with older/alternative compilers
+  #define __has_builtin(x) 0
 #endif
 
 #if __has_builtin(__builtin_unreachable)
