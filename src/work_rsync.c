a6c5419700134b844bc1d21746378fd770a502e0
lucas-mior <lucas.mior@tutamail.com>
2026-07-26 13:41:01 -0300

    style (whitespace)

--- src/work_rsync.c
+++ src/work_rsync.c
@@ -706,8 +706,7 @@ work_rsync(void *user_data) {
         }
 
         if (task->action == ACTION_HARDLINK) {
-            if ((hash_lookup_inode_map(traversal->inode_map,
-                                        &task->file_id, &hardlinks))) {
+            if ((hash_lookup_inode_map(traversal->inode_map, &task->file_id, &hardlinks))) {
                 for (int32 j = 0; j < hardlinks.count; j += 1) {
                     char *link_name = hardlinks.names[j];
                     write_len = hardlinks.names_lens[j];
