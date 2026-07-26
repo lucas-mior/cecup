b582543e7db4bdcd0aa68292b06fe507cdce259a
lucas-mior <lucas.mior@tutamail.com>
2026-07-26 15:08:45 -0300

    wip includes

--- cbase/util.c
+++ cbase/util.c
@@ -10,23 +10,25 @@
 #pragma GCC diagnostic ignored "-Wunused-variable"
 #endif
 
+#include <ctype.h>
+#include <dirent.h>
 #include <errno.h>
-#include <stdio.h>
-#include <stdbool.h>
+#include <fcntl.h>
+#include <float.h>
+#include <fts.h>
+#include <libgen.h>
+#include <limits.h>
+#include <pthread.h>
 #include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/time.h>
-#include <libgen.h>
-#include <pthread.h>
-#include <limits.h>
 #include <sys/stat.h>
-#include <float.h>
-#include <dirent.h>
-#include <ctype.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "platform_detection.h"
 #include "base_macros.h"
