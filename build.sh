aafee7e424582d7c7cb5b80bd78e52ca15a68a6c
lucas-mior <lucas.mior@tutamail.com>
2026-07-26 18:37:18 -0300

    wip

--- build.sh
+++ build.sh
@@ -181,7 +181,7 @@ case "$target" in
     CFLAGS="$CFLAGS $GNUSOURCE -O2 -flto -march=native -ftree-vectorize"
     ;;
 "release")
-    CFLAGS="$CFLAGS $GNUSOURCE -DRELEASING=1 -O2 -flto -march=native -ftree-vectorize"
+    CFLAGS="$CFLAGS $GNUSOURCE -O2 -flto -march=native -ftree-vectorize"
     ;;
 "fast_feedback")
     CC=clang
