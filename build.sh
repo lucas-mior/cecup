77106405feb782cd0e3178f2675bea8de1af51c9
lucas-mior <lucas.mior@tutamail.com>
2026-08-08 13:07:44 -0300

    cleanup

--- build.sh
+++ build.sh
@@ -158,7 +158,6 @@ CFLAGS="$CFLAGS -Werror=all -Werror=extra"
 CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 CFLAGS="$CFLAGS -Wno-cast-qual"
 CFLAGS="$CFLAGS -Wno-deprecated-declarations"
-CFLAGS="$CFLAGS -Wno-unknown-pragmas"
 CFLAGS="$CFLAGS -Wno-unused-macros"
 
 if [ "$CC" = "clang" ]; then
