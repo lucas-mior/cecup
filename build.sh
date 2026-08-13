f869d4ef7dd54c196eb2d22f6e36a67a4d2e9906
lucas-mior <lucas.mior@tutamail.com>
2026-08-13 16:34:34 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -96,7 +96,7 @@ CFLAGS="$CFLAGS -Werror=all -Werror=extra"
 CFLAGS="$CFLAGS -Wno-deprecated-declarations"
 CFLAGS="$CFLAGS -Wno-unused-function"
 
-if [ "$CC" = "clang" ]; then
+if [ "$CC" = "clang" ] || [ "$CC" = "zig cc" ]; then
     CFLAGS="$CFLAGS -Weverything"
     CFLAGS="$CFLAGS -Wno-assign-enum"
     CFLAGS="$CFLAGS -Wno-c++-keyword"
