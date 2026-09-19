b06e85f95b5f55a565460656d66a15e22a813a66
lucas-mior <lucas.mior@tutamail.com>
2026-09-19 19:06:39 -0300

    wip

--- src/on_path.c
+++ src/on_path.c
@@ -442,7 +442,7 @@ main(void) {
 
         SNPRINTF(new_file_full, "%s/%s", src_dir, new_name);
         ASSERT(!access(new_file_full, F_OK));
-        ASSERT(access(src_file_full, F_OK) == -1);
+        ASSERT(access(src_file_full, F_OK));
         ASSERT(cecup.preview_dirty == true);
     }
 
